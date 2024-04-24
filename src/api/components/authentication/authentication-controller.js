104ece43f2eead6f53b4fcf5aa849e8fbf679d94
MakotoMachiYuki <joshua1412a.o.t@gmail.com>
2024-04-24 20:20:09 +0700

    attempt login

--- src/api/components/authentication/authentication-controller.js
+++ src/api/components/authentication/authentication-controller.js
@@ -8,10 +8,34 @@ const authenticationServices = require('./authentication-service');
  * @param {object} next - Express route middlewares
  * @returns {object} Response object or pass an error to the next route
  */
+
+var loginAttempts = 0;
+var maxAttempts = 5;
+var accountLocked = false;
+var lockedTimer = null;
+var timeLimit = 30; // in minutes
 async function login(request, response, next) {
   const { email, password } = request.body;
 
   try {
+    if (accountLocked && Date.now() < lockedTimer) {
+      loginAttempts = 0;
+      const timeLeft = Math.ceil((lockedTimer - Date.now()) / (1000 * 60));
+      return response.status(403).json({
+        error: `Too Many Login Attempts! ${timeLeft} minutes left until you can try it again :)`,
+      });
+    }
+
+    if (loginAttempts >= maxAttempts) {
+      accountLocked = true;
+      lockedTimer = Date.now() + 1000 * 60 * timeLimit;
+
+      return response.status(403).json({
+        error:
+          'Attempts Limit Reached! Now account lock down for 30 minutes! :)',
+      });
+    }
+
     // Check login credentials
     const loginSuccess = await authenticationServices.checkLoginCredentials(
       email,
@@ -19,12 +43,13 @@ async function login(request, response, next) {
     );
 
     if (!loginSuccess) {
-      throw errorResponder(
-        errorTypes.INVALID_CREDENTIALS,
-        'Wrong email or password'
-      );
+      loginAttempts += 1;
+      return response.status(403).json({
+        error: `Invalid email or password!. Please try again! Attempts: ${loginAttempts}`,
+      });
     }
 
+    loginAttempts = 0;
     return response.status(200).json(loginSuccess);
   } catch (error) {
     return next(error);
